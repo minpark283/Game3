ced5171b76b66f44586048eb095c70575a6529e7
minpark283 <parkm4@rpi.edu>
2018-10-28 01:24:55 -0400

    Added Super basic movement for player

---
+++ CharcoalGame/Assets/Scripts/Scr_Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_Player : MonoBehaviour {
+
+    public float player_speed;
+    private Rigidbody player_body;
+    private float moveX;
+    private float moveY;
+
+    // Use this for initialization
+    void Start () {
+        player_body = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        moveX = Input.GetAxis("Horizontal");
+        moveY  = Input.GetAxis("Vertical");
+
+        player_body.velocity = new Vector3(moveX * player_speed, 0, moveY * player_speed );
+
+    }
+}
