2d533b3d58c02604ad7cd06b4fa154522f2cd0f3
minpark283 <parkm4@rpi.edu>
2018-10-28 02:53:49 -0400

    Added basic terrains

---
+++ CharcoalGame/Assets/Scripts/Scr_Maincamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_Maincamera : MonoBehaviour {
+    public GameObject target;
+    Vector3 cameraoffset;
+    // Use this for initialization
+    void Start () {
+        cameraoffset = transform.position - target.transform.position;
+    }
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        Vector3 desiredPosition = target.transform.position + cameraoffset;
+        transform.position = desiredPosition;
+        transform.LookAt(target.transform);
+	}
+}
