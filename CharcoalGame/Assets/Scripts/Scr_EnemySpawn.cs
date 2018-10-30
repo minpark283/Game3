bb79d2210cea2409c099ce9d7b4805c78b97577d
minpark283 <parkm4@rpi.edu>
2018-10-30 04:02:01 -0400

    Made few placeholder object and have basic enemyspawn code

---
+++ CharcoalGame/Assets/Scripts/Scr_EnemySpawn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_EnemySpawn : MonoBehaviour {
+    public GameObject enemySpawn;
+    public List<GameObject> spawnList;
+
+    //Spawn Data
+    public float numSpawn;
+    public float timeBetweenSpawn;
+
+	// Use this for initialization
+	void Start () {
+        spawnList = new List<GameObject>();
+	}
+	
+	public IEnumerator spawn(Vector3 location, float numofSpawn )
+    {
+        for(int i = 0; i < numofSpawn; i++)
+        {
+            Debug.Log("Spawn!");
+            GameObject s = (GameObject)Instantiate(enemySpawn, location, Quaternion.identity);
+            yield return new WaitForSeconds(timeBetweenSpawn);
+        }
+
+    }
+	
+}
