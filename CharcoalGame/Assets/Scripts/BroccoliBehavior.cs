63c090189eb410ad76900626e20e01ceee6f9c41
Alex Weisenbach <weisea3@rpi.edu>
2018-10-31 01:34:44 -0400

    Initial version of pathfinding grid generation
    mapper object/script, script to attach to obstacles

---
+++ CharcoalGame/Assets/Scripts/BroccoliBehavior.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BroccoliBehavior : MonoBehaviour {
+
+    public float speed;
+    public GameObject player;
+
+    public int health;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Hit(int damage)
+    {
+        //Deal damage and check for death
+        health = health - damage;
+        if(health <= 0)
+        {
+            //play a death animation maybe?
+            this.Die();
+        }
+        //Possibly a hit animation?
+    }
+
+    void Die()
+    {
+        //Put other stuff, like animations, in here
+        GameObject.Destroy(this);
+    }
+}
