a8d70d00c0a00c1e700ea11f22331fcd1bcc5db0
minpark283 <parkm4@rpi.edu>
2018-10-31 15:37:05 -0400

    Added winscreen and titlescreen

--- CharcoalGame/Assets/Scripts/Scr_EnemySpawn.cs
+++ CharcoalGame/Assets/Scripts/Scr_EnemySpawn.cs
@@ -23,25 +23,25 @@ public class Scr_EnemySpawn : MonoBehaviour {
         switch (quad)
         {
             case 1:
-                location.x = (Random.Range(1.25f * (landdimension.x / 2), (landdimension.x)));
-                location.z = (Random.Range(1.25f * (landdimension.z / 2), (landdimension.z)));
+                location.x = (Random.Range(1.25f * (landdimension.x / 2), (landdimension.x)) -7);
+                location.z = (Random.Range(1.25f * (landdimension.z / 2), (landdimension.z)) - 7);
                 break;
             case 2:
-                location.x = (Random.Range(0, .75f * (landdimension.x / 2)));
-                location.z = (Random.Range(1.25f * (landdimension.z / 2), (landdimension.z)));
+                location.x = (Random.Range(7, .75f * (landdimension.x / 2)));
+                location.z = (Random.Range(1.25f * (landdimension.z / 2), (landdimension.z)) -7);
                 break;
             case 3:
-                location.x = (Random.Range(0, .75f * (landdimension.x / 2)));
-                location.z = (Random.Range(0, .75f * (landdimension.z /2)));
+                location.x = (Random.Range(7, .75f * (landdimension.x / 2)));
+                location.z = (Random.Range(7, .75f * (landdimension.z /2)));
                 break;
             case 4:
-                location.x = (Random.Range(1.25f * (landdimension.x / 2), (landdimension.x)));
-                location.z = (Random.Range(0, .75f * (landdimension.z / 2)));
+                location.x = (Random.Range(1.25f * (landdimension.x / 2), (landdimension.x)) -7);
+                location.z = (Random.Range(7, .75f * (landdimension.z / 2)));
                 break;
         }
         for (int i = 0; i < numofSpawn; i++)
         {
-            Debug.Log("Spawn!");
+            
             Vector3 randomize = new Vector3(Random.Range(-5, 5), 0, Random.Range(-5, 5));
             location = location + randomize;
             GameObject s = (GameObject)Instantiate(enemySpawn, location, Quaternion.identity);
