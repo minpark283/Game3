e20e61eb09df82d4d24fd9ff31e2cc652ea0e3bd
Nicholas Smith <smithn10@rpi.edu>
2018-10-31 10:21:22 -0400

    Added player jump

--- CharcoalGame/Assets/Scripts/playerMovement 1.cs
+++ CharcoalGame/Assets/Scripts/playerMovement 1.cs
@@ -5,9 +5,20 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour {
     public float speed;
     public Transform cameraTransform;
-	
-	// Update is called once per frame
-	void Update () {
+    public Rigidbody rb;
+    public Collider groundCollider;
+    public float jumpPower;
+    float distToGround;
+
+    // Use this for initialization
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+        groundCollider = GetComponent<Collider>();
+        distToGround = groundCollider.bounds.extents.y;
+    }
+
+    // Update is called once per frame
+    void Update () {
         // get horizontal and vertical inputs for player movement
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
@@ -21,7 +32,16 @@ public class playerMovement : MonoBehaviour {
         right.y = 0;
         forward = forward.normalized;
         right = right.normalized;
-        
         transform.position += (forward * movement.y + right * movement.x) * Time.deltaTime * speed;
-	}
+
+        // player jump
+        if (Input.GetKeyDown(KeyCode.Space) && (isGrounded())) {
+            rb.AddForce(0, jumpPower, 0, ForceMode.Impulse); // impulse 
+        }
+    }
+
+    // raycast vector below player to check for ground
+    bool isGrounded() {
+        return Physics.Raycast(transform.position, -Vector3.up, distToGround);
+    }
 }
