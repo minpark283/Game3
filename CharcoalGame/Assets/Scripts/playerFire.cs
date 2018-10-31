78d562239e1718267fef81a9f621fc70dd0d9ac0
Nicholas Smith <smithn10@rpi.edu>
2018-10-31 16:51:44 -0400

    Added player projectile

---
+++ CharcoalGame/Assets/Scripts/playerFire.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerFire : MonoBehaviour {
+    public float fireCost;
+    public float fireDamage;
+    public float fireSpeed;
+    public float fireRangeTimer;
+    public GameObject projectileObject;
+    private GameObject player;
+    private playerGlobals globals;
+
+    // Use this for initialization
+    void Start() {
+        player = GameObject.Find("Player");
+        globals = player.GetComponent<playerGlobals>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetMouseButtonDown(0) && (globals.fuel > 0)) {
+            GameObject projectile = (GameObject)Instantiate(projectileObject, player.transform.position, player.transform.rotation);
+            projectile.layer = 12;
+            projectile.GetComponent<Rigidbody>().velocity = projectile.transform.forward * fireSpeed;
+            Destroy(projectile, fireRangeTimer);
+
+            globals.fuel -= fireCost;
+        }
+    }
+}
