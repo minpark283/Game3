3fc90d1304ab0247d1c1cd83e750ad3bb4606b74
minpark283 <parkm4@rpi.edu>
2018-10-31 14:50:17 -0400

    Enemy Spawn works now

--- CharcoalGame/Assets/Scripts/Scr_EnemySpawn.cs
+++ CharcoalGame/Assets/Scripts/Scr_EnemySpawn.cs
@@ -5,21 +5,45 @@ using UnityEngine;
 public class Scr_EnemySpawn : MonoBehaviour {
     public GameObject enemySpawn;
     public List<GameObject> spawnList;
-
+    public Terrain land;
+    public Vector3 landdimension;
     //Spawn Data
     public float numSpawn;
     public float timeBetweenSpawn;
 
 	// Use this for initialization
 	void Start () {
+        landdimension = land.terrainData.size;
         spawnList = new List<GameObject>();
 	}
 	
-	public IEnumerator spawn(Vector3 location, float numofSpawn )
+	public IEnumerator spawn(float numofSpawn, int quad)
     {
-        for(int i = 0; i < numofSpawn; i++)
+        Vector3 location = new Vector3(0, 0, 0);
+        switch (quad)
+        {
+            case 1:
+                location.x = (Random.Range(1.25f * (landdimension.x / 2), (landdimension.x)));
+                location.z = (Random.Range(1.25f * (landdimension.z / 2), (landdimension.z)));
+                break;
+            case 2:
+                location.x = (Random.Range(0, .75f * (landdimension.x / 2)));
+                location.z = (Random.Range(1.25f * (landdimension.z / 2), (landdimension.z)));
+                break;
+            case 3:
+                location.x = (Random.Range(0, .75f * (landdimension.x / 2)));
+                location.z = (Random.Range(0, .75f * (landdimension.z /2)));
+                break;
+            case 4:
+                location.x = (Random.Range(1.25f * (landdimension.x / 2), (landdimension.x)));
+                location.z = (Random.Range(0, .75f * (landdimension.z / 2)));
+                break;
+        }
+        for (int i = 0; i < numofSpawn; i++)
         {
             Debug.Log("Spawn!");
+            Vector3 randomize = new Vector3(Random.Range(-7, 7), 0, Random.Range(-7, 7));
+            location = location + randomize;
             GameObject s = (GameObject)Instantiate(enemySpawn, location, Quaternion.identity);
             yield return new WaitForSeconds(timeBetweenSpawn);
         }
