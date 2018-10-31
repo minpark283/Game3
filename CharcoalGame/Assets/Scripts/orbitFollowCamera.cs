a1ee83c4b6eef883f4addd07c8085f8d1ee07fcf
Nicholas Smith <smithn10@rpi.edu>
2018-10-31 09:49:36 -0400

    Added movement script, camera script, and camera prefab for settings

---
+++ CharcoalGame/Assets/Scripts/orbitFollowCamera.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class orbitFollowCamera : MonoBehaviour {
+    public Transform playerTransform;
+    private Vector3 cameraOffset;
+    public float orbitSpeed;
+    [Range(0.01f, 1.00f)]
+    public float smoothness;
+
+
+	// Use this for initialization
+	void Start () {
+        cameraOffset = transform.position - playerTransform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // follow player by camera offset
+        Vector3 positionUpdate = playerTransform.position + cameraOffset;
+        transform.position = Vector3.Slerp(transform.position, positionUpdate, smoothness);
+
+        // allow camera to orbit horizontally
+        float scrollMovement = Input.GetAxis("Mouse X") * Time.deltaTime * orbitSpeed;
+        Quaternion turnCameraAngle = Quaternion.AngleAxis(scrollMovement, Vector3.up);
+        cameraOffset = turnCameraAngle * cameraOffset;
+
+        // set player's Y rotation to the camera's Y rotation
+        float cameraRotationY = transform.eulerAngles.y;
+        playerTransform.eulerAngles = new Vector3(0f, cameraRotationY, 0f);
+        transform.LookAt(playerTransform);
+	}
+}
---
+++ CharcoalGame/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+    public float speed;
+    public Transform cameraTransform;
+	
+	// Update is called once per frame
+	void Update () {
+        // get horizontal and vertical inputs for player movement
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(horizontal, vertical);
+        movement = Vector2.ClampMagnitude(movement, 1);
+
+        // use the cameras position for direction inputs instead of world position for direction inputs
+        Vector3 forward = cameraTransform.forward;
+        Vector3 right = cameraTransform.right;
+        forward.y = 0;
+        right.y = 0;
+        forward = forward.normalized;
+        right = right.normalized;
+        
+        transform.position += (forward * movement.y + right * movement.x) * Time.deltaTime * speed;
+	}
+}
