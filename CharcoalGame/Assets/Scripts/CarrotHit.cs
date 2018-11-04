e985bb0c0482a7786cb08bcfe3ba24d73ec5ee6d
Alex Weisenbach <weisea3@rpi.edu>
2018-11-04 16:44:22 -0500

    changes to enemies dealing damage to player

--- CharcoalGame/Assets/Scripts/CarrotHit.cs
+++ CharcoalGame/Assets/Scripts/CarrotHit.cs
@@ -2,24 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BroccoliHitScript : MonoBehaviour {
+public class CarrotHit : MonoBehaviour {
 
     public float damage;
     public bool alreadyHit;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     void OnTriggerEnter(Collider other)
     {
-        
-        if(other.gameObject.tag.Equals("Player") && !alreadyHit)
+
+        if (other.gameObject.tag.Equals("Player") && !alreadyHit)
         {
             print("hit");
             alreadyHit = true;
