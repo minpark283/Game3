a499d26ca380c44ea144c55169aa200f258a8684
Nicholas Smith <smithn10@rpi.edu>
2018-11-06 21:10:15 -0500

    Fixed CharcoalSpawner

--- CharcoalGame/Assets/Scripts/charcoalSpawner.cs
+++ CharcoalGame/Assets/Scripts/charcoalSpawner.cs
@@ -7,31 +7,98 @@ public class charcoalSpawner : MonoBehaviour {
     public int maxNumberOfCharcoal;
     public int currentNumberOfCharcoal;
     public GameObject charcoalObject;
+    private int currentQuadrant;
+    private Dictionary<int, List<Vector3>> spawnPointDict;
 
     // Use this for initialization
     void Start () {
         spawnPointList = new List<Vector3>();
-        spawnPointList.Add(new Vector3(100, 0, 100));
-        spawnPointList.Add(new Vector3(80, 0, 100));
-        spawnPointList.Add(new Vector3(60, 0, 100));
-        spawnPointList.Add(new Vector3(40, 0, 100));
-        spawnPointList.Add(new Vector3(20, 0, 100));
-        spawnPointList.Add(new Vector3(20, 0, 20));
-        spawnPointList.Add(new Vector3(100, 0, 80));
-        spawnPointList.Add(new Vector3(100, 0, 60));
-        spawnPointList.Add(new Vector3(100, 0, 40));
-        spawnPointList.Add(new Vector3(100, 0, 20));
+        spawnPointDict = new Dictionary<int, List<Vector3>>();
         currentNumberOfCharcoal = 0;
+
+        // bottom left quadrant (0)
+        spawnPointList.Add(new Vector3(50, 0, 50));
+        spawnPointList.Add(new Vector3(10, 0, 40));
+        spawnPointList.Add(new Vector3(40, 0, 70));
+        spawnPointList.Add(new Vector3(60, 0, 35));
+        spawnPointDict.Add(0, spawnPointList);
+        spawnPointList.Clear();
+
+        // bottom middle quadrant (1)
+        spawnPointList.Add(new Vector3(160, 0, 85));
+        spawnPointList.Add(new Vector3(180, 0, 65));
+        spawnPointList.Add(new Vector3(118, 0, 54));
+        spawnPointList.Add(new Vector3(140, 0, 15));
+        spawnPointDict.Add(1, spawnPointList);
+        spawnPointList.Clear();
+
+        // bottom right quadrant (2)
+        spawnPointList.Add(new Vector3(220, 0, 70));
+        spawnPointList.Add(new Vector3(230, 0, 20));
+        spawnPointList.Add(new Vector3(270, 0, 35));
+        spawnPointList.Add(new Vector3(275, 0, 65));
+        spawnPointDict.Add(2, spawnPointList);
+        spawnPointList.Clear();
+
+        // middle left quadrant (3)
+        spawnPointList.Add(new Vector3(61, 0, 70));
+        spawnPointList.Add(new Vector3(60, 0, 130));
+        spawnPointList.Add(new Vector3(25, 0, 150));
+        spawnPointDict.Add(3, spawnPointList);
+        spawnPointList.Clear();
+
+        // middle center quadrant (4)
+        spawnPointList.Add(new Vector3(150, 0, 175));
+        spawnPointList.Add(new Vector3(180, 0, 175));
+        spawnPointList.Add(new Vector3(185, 0, 120));
+        spawnPointList.Add(new Vector3(113, 0, 120));
+        spawnPointList.Add(new Vector3(113, 0, 176));
+        spawnPointDict.Add(4, spawnPointList);
+        spawnPointList.Clear();
+
+        // middle right quadrant (5)
+        spawnPointList.Add(new Vector3(280, 0, 175));
+        spawnPointList.Add(new Vector3(280, 0, 125));
+        spawnPointList.Add(new Vector3(235, 0, 130));
+        spawnPointDict.Add(5, spawnPointList);
+        spawnPointList.Clear();
+
+        // top left quadrant (6)
+        spawnPointList.Add(new Vector3(60, 0, 255));
+        spawnPointList.Add(new Vector3(35, 0, 220));
+        spawnPointList.Add(new Vector3(82, 0, 220));
+        spawnPointDict.Add(6, spawnPointList);
+        spawnPointList.Clear();
+
+        // top middle quadrant (7)
+        spawnPointList.Add(new Vector3(150, 0, 260));
+        spawnPointList.Add(new Vector3(130, 0, 230));
+        spawnPointList.Add(new Vector3(170, 0, 230));
+        spawnPointList.Add(new Vector3(150, 0, 230));
+        spawnPointDict.Add(7, spawnPointList);
+        spawnPointList.Clear();
+
+        // top right quadrant (8)
+        spawnPointList.Add(new Vector3(224, 0, 230));
+        spawnPointList.Add(new Vector3(240, 0, 200));
+        spawnPointList.Add(new Vector3(240, 0, 270));
+        spawnPointList.Add(new Vector3(280, 0, 240));
+        spawnPointDict.Add(8, spawnPointList);
+        spawnPointList.Clear();
     }
 	
 	// Update is called once per frame
 	void Update () {
+        List<Vector3> spawnQuadrantPointList;
 		if(currentNumberOfCharcoal < maxNumberOfCharcoal) {
-            int randomIndex = Random.Range(0, spawnPointList.Count);
-            Vector3 spawnPoint = spawnPointList[randomIndex];
-            GameObject charcoal = (GameObject)Instantiate(charcoalObject, spawnPoint, transform.rotation);
-            charcoal.gameObject.layer = 14;
-            currentNumberOfCharcoal++;
+            currentQuadrant = Random.Range(0, 8);
+            if(spawnPointDict.TryGetValue(currentQuadrant, out spawnQuadrantPointList)) {
+                int randomIndex = Random.Range(0, spawnQuadrantPointList.Count);
+                Vector3 spawnPoint = spawnQuadrantPointList[randomIndex];
+                GameObject charcoal = (GameObject)Instantiate(charcoalObject, spawnPoint, transform.rotation);
+                charcoal.gameObject.layer = 14;
+                currentNumberOfCharcoal++;
+            }
         }
 	}
 }
