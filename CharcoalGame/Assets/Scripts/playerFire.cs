842dabb3974a94a8e610d41c92e90199914b8487
minpark283 <parkm4@rpi.edu>
2018-11-07 10:24:38 -0500

    Fixed bunch of enemy data stuff

--- CharcoalGame/Assets/Scripts/playerFire.cs
+++ CharcoalGame/Assets/Scripts/playerFire.cs
@@ -11,7 +11,7 @@ public class playerFire : MonoBehaviour {
     public GameObject projectileObject;
     public AudioClip shootSound;
     public AudioSource audioSource;
-    public ParticleSystem particles;
+
     private playerGlobals globals;
 
     // Use this for initialization
@@ -23,12 +23,11 @@ public class playerFire : MonoBehaviour {
     void Update() {
         if (Input.GetMouseButtonDown(0) && (globals.fuel >= fireCost)) {
             GameObject projectile = (GameObject)Instantiate(projectileObject, transform.position, transform.rotation);
-            ParticleSystem party = (ParticleSystem)Instantiate(particles, projectile.transform.position, projectile.transform.rotation);
             
             projectile.layer = 13;
             projectile.GetComponent<Rigidbody>().velocity = projectile.transform.forward * fireSpeed;
             Destroy(projectile, fireRangeTimer);
-            Destroy(party, fireRangeTimer);
+          
             Physics.IgnoreCollision(GetComponent<Collider>(), projectile.GetComponent<Collider>());
 
             audioSource.PlayOneShot(shootSound, volume);
