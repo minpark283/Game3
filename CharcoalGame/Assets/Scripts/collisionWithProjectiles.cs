de615501ca532fdc65757b66a5eb670ae189df0c
Nicholas Smith <smithn10@rpi.edu>
2018-11-07 11:25:40 -0500

    Added trees and prefab polishing

--- CharcoalGame/Assets/Scripts/collisionWithProjectiles.cs
+++ CharcoalGame/Assets/Scripts/collisionWithProjectiles.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class collisionWithPeapodShot : MonoBehaviour {
+public class collisionWithProjectiles : MonoBehaviour {
     public AudioClip splatSound;
     public AudioSource audioSource;
 
